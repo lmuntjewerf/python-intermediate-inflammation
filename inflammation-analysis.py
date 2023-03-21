580e4d77cf0a89eaadf7c322a1d0c60d1141adad
Laura Muntjewerf <laura.muntjewerf@gmail.com>
2023-03-21 13:18:43 +0000

    Code style fixes.

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""Software for managing and analysing patients' inflammation data in our imaginary hospital."""
+"""
+Software for managing and analysing patients' 
+inflammation data in our imaginary hospital.
+"""
 
 import argparse
-
 from inflammation import models, views
-
 
 def main(args):
     """The MVC Controller of the patient inflammation data system.
@@ -13,15 +14,18 @@ def main(args):
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    InFiles = args.infiles
-    if not isinstance(InFiles, list):
-        InFiles = [args.infiles]
+    infiles = args.infiles
+    if not isinstance(infiles, list):
+        infiles = [args.infiles]
 
-
-    for filename in InFiles:
+    for filename in infiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {
+            'average': models.daily_mean(inflammation_data), 
+            'max': models.daily_max(inflammation_data), 
+            'min': models.daily_min(inflammation_data)
+            }
 
         views.visualize(view_data)
 
